b401dbf7279832b813ead7825467aeab7860fc80
Lukáš Hozda <luk.hozda@gmail.com>
2025-01-16 16:52:37 +0100

    guix chad shit

--- manifest.scm
+++ manifest.scm
@@ -2,6 +2,7 @@
 (use-modules (guix packages)
              (gnu packages scheme)
              (gnu packages guile)
+             (gnu packages linux)
              (guix gexp)
              (guix licenses)
              (guix build-system trivial)
@@ -13,7 +14,7 @@
    (version "0.1")
    (source #f)
    (build-system trivial-build-system)
-   (inputs (cons* guile-3.0 extra-inputs))  ; Always include guile + extras
+   (propagated-inputs (cons* guile-3.0 inotify-tools extra-inputs))  ; Changed inputs to propagated-inputs
    (arguments
     (list
      #:builder
@@ -32,18 +33,50 @@
    (license expat)
    (home-page #f)))
 
-(define develop-script
+(define (make-dev-script project-name)
   (make-guile-script
    "develop"
-   ''(let loop ()
-       (display "Rebuilding...\n")
-       (system* "gerbil" "build")
-       (display "Running...\n")
-       (system* (string-append ".gerbil/bin/" "your-app-name"))
-       (sleep 2)
-       (loop))))
+   `'(let ((project-dir ,(string-append "./" project-name))
+           (binary-path ,(string-append ".gerbil/bin/" project-name))
+           (server-pid #f))
+
+       (use-modules (ice-9 popen)
+                    (ice-9 rdelim))
+
+       (letrec ((kill-server
+                 (lambda ()
+                   (when server-pid
+                     (system* "kill" (number->string server-pid)))))
+
+                (start-server
+                 (lambda ()
+                   (kill-server)
+                   (let ((pid (primitive-fork)))
+                     (if (zero? pid)
+                         (begin
+                           (system* binary-path)
+                           (exit 0))
+                         (set! server-pid pid)))))
+
+                (rebuild-and-restart
+                 (lambda ()
+                   (display "Rebuilding...\n")
+                   (system* "gerbil" "build")
+                   (display "Restarting server...\n")
+                   (start-server))))
+
+         (rebuild-and-restart)
+
+         (let ((port (open-input-pipe
+                      (string-append "inotifywait -m -r -e modify,create,delete "
+                                     project-dir))))
+           (let loop ()
+             (let ((line (read-line port)))
+               (when (not (eof-object? line))
+                 (rebuild-and-restart)
+                 (loop)))))))))
 
 (packages->manifest
  (list gerbil
        gambit-c
-       develop-script))
+       (make-dev-script "qons")))
